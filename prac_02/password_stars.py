c8efd3586b93507c271f111e361b77356a8c51cf
Jun <140615899+JL-38@users.noreply.github.com>
2023-09-23 20:43:38 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,29 @@
-password_length = 10
-password = input("Password: ")
-while len(password) < password_length:
-    print("Invalid password!")
+def main():
+    """Get and print password using a funtion."""
+    password_length = 10
+    password = get_password(password_length)
+    print(len(password) * '*')
+
+
+def get_password(password_length):
+    """Get password, meets the min length."""
     password = input("Password: ")
-print(len(password) * '*')
+    while len(password) < password_length:
+        print("Invalid password!")
+        password = input("Password: ")
+    return password
+
+
+main()
+
+
+# def main():
+#     password_length = 10
+#     password = input("Password: ")
+#     while len(password) < password_length:
+#         print("Invalid password!")
+#         password = input("Password: ")
+#     print(len(password) * '*')
+#
+#
+# main()
