79c405ed6803d717ee96d88cd8ebe31841cbae62
Jun <140615899+JL-38@users.noreply.github.com>
2023-09-23 23:03:51 +1000

    Refactor password check program to use functions - better layout

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,12 +2,23 @@ PASSWORD_LENGTH = 10
 
 
 def main():
-    """Get and print as many stars as password characters."""
+    """Get and print password using functions."""
+    password = get_password(PASSWORD_LENGTH)
+    print_stars(password)
+
+
+def print_stars(password):
+    """Print as many stars as the password length."""
+    print(len(password) * '*')
+
+
+def get_password(password_length):
+    """Get password, meeting the minimum length requirement."""
     password = input("Password: ")
-    while len(password) < PASSWORD_LENGTH:
+    while len(password) < password_length:
         print("Invalid password!")
         password = input("Password: ")
-    print(len(password) * '*')
+    return password
 
 
 main()
