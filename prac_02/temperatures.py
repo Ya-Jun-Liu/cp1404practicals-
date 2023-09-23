24c2a4f845117923c1ec2c768e4a2f995d676328
Jun <140615899+JL-38@users.noreply.github.com>
2023-09-23 22:16:06 +1000

    Refactor temperature to use conversion functions - for better layout

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -8,14 +8,26 @@ Q - Quit"""
 
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def convert_celsius_to_fahrenheit():
+    """Convert celsius to fahrenheit."""
+    return celsius * 9.0 / 5 + 32
+
+
+def convert_fahrenheit_to_celsius():
+    """Convert fahrenheit to celsius."""
+    return 5 / 9 * (fahrenheit - 32)
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        fahrenheit = convert_celsius_to_fahrenheit()
         print(f"Result: {fahrenheit:.2f} F")
     elif choice == "F":
         fahrenheit = float(input("fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        celsius = convert_fahrenheit_to_celsius()
         print(f"Result: {celsius:.2f} C")
     else:
         print("Invalid option")
