a487c2f7b13b9ce6893ff044f5f8a309af0d6200
Jun <140615899+JL-38@users.noreply.github.com>
2023-09-24 12:06:36 +1000

    Use main function to ask user score, generate a random score,and print results

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,21 +1,22 @@
 """
-CP1404 - Practical
-Broken program to determine score status
+CP1404 - Practical program to determine score status
 """
 
 import random
 
-
 def main():
     """Get user's score and print result."""
+    score = float(input("Score: "))
+    print(f"Entered score {score} is '{determine_status(score)}'.")
+    """Generate random score and print result."""
     score = random.randint(0, 100)
-    print(f"Random score is {score} \nThe status is '{determine_status(score)}'")
+    print(f"Random score is {score}. \nThe status is '{determine_status(score)}'.")
 
 
 def determine_status(score):
     """Determine status of a score."""
     if score < 0 or score > 100:
-        return "Invalid score)"
+        return "Invalid score"
     elif score >= 90:
         return "Excellent"
     elif score >= 50:
