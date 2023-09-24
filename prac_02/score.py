4a400e6ce9783ad00094b1f9d7b69f5877726c39
Jun <140615899+JL-38@users.noreply.github.com>
2023-09-24 11:45:19 +1000

    Use main to generate random score and print the result

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,25 @@ CP1404 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
+import random
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Bad")
+
+def main():
+    """Get user's score and print result."""
+    score = random.randint(0, 100)
+    print(f"Random score is {score} \nThe status is '{determine_status(score)}'")
+
+
+def determine_status(score):
+    """Determine status of a score."""
+    if score < 0 or score > 100:
+        return "Invalid score)"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Pass"
+    else:
+        return "Bad"
+
+
+main()
