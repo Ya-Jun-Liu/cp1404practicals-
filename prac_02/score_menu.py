3f92648ea45cfce8d4ae86c59bec83278f31bcb9
Jun <140615899+JL-38@users.noreply.github.com>
2023-09-24 14:15:26 +1000

    Creat a main function for score menu program

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -25,27 +25,32 @@ while choice != <quit option>
 
 menu_string = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
 
-print(menu_string)
-choice = input("> ").upper()
-while choice != "Q":
-    if choice == "G":
-        score = int(input("Score: "))
-        while score > 100 or score < 0:
-            print("Invalid score")
-            score = int(input("Score: "))
-    elif choice == "P":
-        if score < 0 or score > 100:
-            print("Invalid score")
-        elif score >= 90:
-            print("Excellent")
-        elif score >= 50:
-            print("Pass")
-        else:
-            print("Bad")
-    elif choice == "S":
-        print(score * '*')
-    else:
-        print("Invalid choice")
+
+def main():
     print(menu_string)
     choice = input("> ").upper()
-print("Thank you!")
+    while choice != "Q":
+        if choice == "G":
+            score = int(input("Score: "))
+            while score > 100 or score < 0:
+                print("Invalid score")
+                score = int(input("Score: "))
+        elif choice == "P":
+            if score < 0 or score > 100:
+                print("Invalid score")
+            elif score >= 90:
+                print("Excellent")
+            elif score >= 50:
+                print("Pass")
+            else:
+                print("Bad")
+        elif choice == "S":
+            print(score * '*')
+        else:
+            print("Invalid choice")
+        print(menu_string)
+        choice = input("> ").upper()
+    print("Thank you!")
+
+
+main()
