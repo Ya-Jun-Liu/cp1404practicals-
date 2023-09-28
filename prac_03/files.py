a61da078d4d15015e21d03496ef5d1fb37edbba8
Jun <140615899+JL-38@users.noreply.github.com>
2023-09-28 19:54:31 +1000

    Creat number.txt, open,read,add first 2 lines, print result

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,16 +1,40 @@
-# Q1 Write code that asks the user for their name,
-# then opens a file called "name.txt"
-# and writes that name to it.
-# Remember to close your file.
-out_file = open("name.txt", "w")
-user_name = input("Enter your name: ")
-print(user_name, file = out_file)
-out_file.close()
+"""
+Q1. Pseudocode
+open a file called "name.txt"
+ask the user for their name
+write user's name to the file
+close file.
+"""
 
+# out_file = open("name.txt", "w")
+# user_name = input("Enter your name: ")
+# print(user_name, file=out_file)
+# out_file.close()
+#
+# """
+# Q2. Pseudocode
+# open "name.txt"
+# reads the name (as above)
+# prints "Your name is Bob"
+# """
+#
+# with open("name.txt", "r") as out_file:
+#     for line in out_file:
+#         print(f"\"Your name is {user_name}\"")
 
-# Q2 Write code that opens "name.txt"
-# and reads the name (as above) then prints,
-# "Your name is Bob"
-with open("name.txt", "r") as out_file:
-    for line in out_file:
-        print(f"\"Your name is {user_name}\"")
+"""
+Q3. Pseudocode
+Create a text file called "numbers.txt"
+save it in your prac directory
+open "numbers.txt", 
+read only the first two numbers
+add them together 
+print the result
+"""
+
+in_file = open("numbers.txt", "r")
+total = 0
+number1 = int(in_file.readline())
+number2 = int(in_file.readline())
+in_file.close()
+print(number1 + number2)
