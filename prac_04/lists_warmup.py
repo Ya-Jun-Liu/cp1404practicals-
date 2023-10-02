5c608968567c2675a3e66d43a0aa020751564841
Jun <140615899+JL-38@users.noreply.github.com>
2023-10-02 16:15:10 +1000

    Add lists_warmup

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,35 @@
+"""What values do the following expressions have? """
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers)
+
+numbers[0]  # 3
+numbers[-1]  # 2
+numbers[3]  # 1
+numbers[:-1]  # [3, 1, 4, 1, 5, 9]
+numbers[3:4]  # [1]
+5 in numbers  # True
+7 in numbers  # False
+"3" in numbers  # False
+numbers + [6, 5, 3]  # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+"""Write statements to achieve the following."""
+# 1. Change the first element of numbers to "ten" (the string, not the number 10)
+numbers = [3, 1, 4, 1, 5, 9, 2]
+numbers[0] = "ten"
+print(numbers)
+
+# 2. Change the last element of numbers to 1
+numbers = [3, 1, 4, 1, 5, 9, 2]
+numbers[-1] = 1
+print(numbers)
+
+# 3. Print all the elements from numbers except the first two (slice)
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers[2:])
+
+# 4. Print whether 9 is an element of numbers
+numbers = [3, 1, 4, 1, 5, 9, 2]
+if 9 in numbers:
+    print("9 is an element of numbers")
+else:
+    print("9 is not an element of numbers")
