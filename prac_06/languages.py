b9d6c61da5da80d959821d1c4e523b587fea16f3
Jun <140615899+JL-38@users.noreply.github.com>
2023-10-31 18:14:12 +1000

    Add car details to languages.py

---
+++ prac_06/languages.py
@@ -0,0 +1,10 @@
+"""
+languages
+Estimate: 30 minutes
+Actual: 60 minutes
+"""
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
