6e74cf904b6ae74d07f94c7273198727ca32eaca
Jun <140615899+JL-38@users.noreply.github.com>
2023-10-31 18:59:57 +1000

    Print names of all the languages with dynamic typing

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -3,8 +3,24 @@ languages
 Estimate: 30 minutes
 Actual: 60 minutes
 """
+from prac_06.programming_language import ProgrammingLanguage
 
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-print(python)
+
+# import the class from file
+
+
+def main():
+    """Print the dynamically typed languages from the file."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [python, ruby, visual_basic]
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
