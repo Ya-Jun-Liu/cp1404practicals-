622ff17651cb23d699b8bfb99b20630944c4abd9
Jun <140615899+JL-38@users.noreply.github.com>
2023-11-05 02:24:38 +1000

    Add functions to project_management

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -16,13 +16,31 @@ PRIORITY = 'priority'
 COST_ESTIMATE = 'cost_estimate'
 COMPLETE_PERCENTAGE = 'complete_percentage'
 
-# Define the rules that input should be not violated
-STR_RULE = {lambda s: len(s) != 0: "Input can not be blank."}
-PRIORITY_RULE = {lambda y: y > 0: "Priority should be greater than 0."}
-COST_RULE = {lambda y: y > 0: "Cost estimate should be greater than 0."}
-COMP_RULE = {lambda y: 100 >= y >=
-             0: "Completion Percentage should be in ragne [0,100]"}
 
+# Define the rules that input should be not violated
+def is_not_blank(s):
+    """Set condition:not blank"""
+    return len(s) != 0
+
+
+STR_RULE = {is_not_blank: "Input can not be blank."}
+
+
+def gt_zero(y):
+    """Set condition: greater than zero."""
+    return y > 0
+
+
+PRIORITY_RULE = {gt_zero: "Priority should be greater than 0."}
+COST_RULE = {gt_zero: "Cost estimate should be greater than 0."}
+
+
+def is_percentage(p):
+    """Define percentage parameter."""
+    return 100 >= p >= 0
+
+
+COMP_RULE = {is_percentage: "Completion Percentage should be in range [0,100]"}
 
 
 def str_to_date(date_string):
@@ -34,8 +52,6 @@ def str_to_date(date_string):
 def date_to_str(date):
     """Convert a date to a date string in the specified format."""
     return date.strftime("%d/%m/%Y")
-
-
 
 
 def main():
@@ -57,7 +73,40 @@ def main():
         print("- (U)pdate project")
         print("- (Q)uit")
 
+        # Input prompt
+        choice = input(">>> ").upper()
+        if choice == "L":
+            # Load csv, original projects will be discarded.
+            projects = myinput("Enter a path to be loaded: ", load_csv_file)
+        elif choice == 'S':
+            # Save csv file
 
+            # To input repeatedly, we write a mapping function that can
+            # save csv file from only filename
+            def save_csv_file2(filename):
+                save_csv_file(projects, filename)
+
+            filename = myinput("Enter a path to be saved: ", save_csv_file2)
+        elif choice == 'Q':
+            # Quit - Save csv to original file (FILENAME)
+            save_csv_file(projects, FILENAME)
+            # if quit, break the inf loop and exit the program.
+            user_quit = True
+        elif choice == 'D':
+            # Display
+            display_project(projects)
+        elif choice == 'A':
+            # Add new project
+            add_new_project(projects)
+        elif choice == 'U':
+            # Update precentage
+            update_project(projects)
+        elif choice == 'F':
+            # Filter projects
+            filter_project(projects)
+        elif choice:
+            print(f"GET '{choice}'")
+            print("Invalid manu choice")
 
 
 if __name__ == '__main__':
