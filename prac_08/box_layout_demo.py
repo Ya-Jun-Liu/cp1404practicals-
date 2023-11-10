9e18a1f86479bb17f6fd85717d494a445b57311c
Jun <140615899+JL-38@users.noreply.github.com>
2023-11-10 13:06:54 +1000

    Add box_layout.kv/py program

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,19 +4,9 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
-        """Create a root widget."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
-
-    def handle_greet(self):
-        """Handle greeting event."""
-        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
-
-    def clear_field(self):
-        """Clear the text field and the output label to blank."""
-        self.root.ids.input_name.text = ""
-        self.root.ids.output_label.text = ""
 
 
 BoxLayoutDemo().run()
