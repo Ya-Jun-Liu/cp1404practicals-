826537e19866234075d7e9b0b5ecbf4766279db6
Jun <140615899+JL-38@users.noreply.github.com>
2023-11-10 11:31:00 +1000

    Modify docstrings and font size for the box_layout program

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,11 +4,13 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Create a root widget."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Handle greeting event."""
         print('test')
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
