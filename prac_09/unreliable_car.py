ac6639bdf0b9e156f7b29e53d874a76077f473e3
Jun <140615899+JL-38@users.noreply.github.com>
2023-11-19 17:57:22 +1000

    Add unreliable_car program

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,20 @@
+from prac_09.car import Car
+from random import randint
+
+
+class UnreliableCar(Car):  # overrides class Car
+    """An unreliable car."""
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive the car if the random number is below the reliability level of the car."""
+        random_number = randint(1, 100)
+        if random_number >= self.reliability:
+            distance = 0
+        distance_driven = super().drive(distance)
+        return distance_driven
+
